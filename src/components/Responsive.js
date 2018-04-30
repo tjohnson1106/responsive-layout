1de6f32544fdf47379456a2c37fefba26b6cbb3a
Thomas_Johnson <tjohnson1106@gmail.com>
2018-04-30 14:51:23 -0400

    changed profile photo view height and width

--- src/components/Responsive.js
+++ src/components/Responsive.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 class Responsive extends Component {
   state = {};
